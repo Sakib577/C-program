4da1b98cf921fea06e34d04611afb05a3a5df902
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2024-03-24 00:04:51 +0600

    Update

--- Practice in lab  and home/Basic declaration and expressions/SakibProblem74.c
+++ Practice in lab  and home/Basic declaration and expressions/SakibProblem74.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-    
+    char str[] = {"abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    printf("List of integer equivalents of letters (a-z, A-Z).\n");
+    printf("==================================================\n");
+
+    for (int i = 0; i < 53; i++)
+    {
+        printf("%3d ", str[i]);
+        if ((i + 1) % 6 == 0)
+        {
+            printf("\n");
+        }
+    }
     return 0;
 }
