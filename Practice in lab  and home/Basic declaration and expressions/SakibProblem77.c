b5b8e63fe18a9f89b018854e4146c28be91a0b6d
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2024-03-24 15:50:55 +0600

    Added

--- Practice in lab  and home/Basic declaration and expressions/SakibProblem77.c
+++ Practice in lab  and home/Basic declaration and expressions/SakibProblem77.c
@@ -2,6 +2,14 @@
 
 int main()
 {
-    
+    int principle, days;
+    float rate, interest;
+    printf("Enter the value of principle, rate and days in sequence:\n");
+    scanf("%d%f%d", &principle, &rate, &days);
+
+    int year = days / 365;
+
+    interest = principle * rate * year;
+    printf("$%.f", interest);
     return 0;
 }
