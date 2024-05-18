efae56f4f6212550f140caca4755bdf3f2cd6543
Abdullah As Sakib <sakib5800.sk.3@gmail.com>
2024-05-18 22:45:06 +0600

    Added

---
+++ Practice in lab  and home/Pattern printing/SakibPascelsTriangle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int coef, row;
+
+printf("Enter the number of row:");
+    scanf("%d", &row);
+
+    for (int n = 0; n < row; n++)
+    {
+        coef =1;
+        for (int i = 0; i < row-n; i++)
+        {
+            printf("  ");
+        }
+        
+
+        for (int a = 0; a <= n; a++)
+        {
+            if (a == 0 || n == 0)
+            {
+                printf("%d   ", coef);
+            }
+            else
+            {
+                coef = coef * (n - a + 1) / a;
+                printf("%d   ", coef);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
